d046ac2a3ca6b81a0dfb2595cf8cfaf33d5b6966
viniciusdsouza <vduartesantiago@gmail.com>
2022-05-22 16:20:44 -0300

    feat: endpoints for menu-api with controllers and services

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,26 @@
  * Required External Modules
  */
 
- import * as dotenv from "dotenv";
- import express from "express";
- import cors from "cors";
- import helmet from "helmet";
- 
- dotenv.config();
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
+
+dotenv.config();
 
 
 /**
  * App Variables
  */
 
- if (!process.env.PORT) {
+if (!process.env.PORT) {
     process.exit(1);
- }
- 
- const PORT: number = parseInt(process.env.PORT as string, 10);
- 
- const app = express();
+}
+
+const PORT: number = parseInt(process.env.PORT as string, 10);
+
+const app = express();
 
 
 /**
@@ -31,15 +32,16 @@
  *  App Configuration
  */
 
- app.use(helmet());
- app.use(cors());
- app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
 
 
 /**
  * Server Activation
  */
 
- app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-  });
+});
